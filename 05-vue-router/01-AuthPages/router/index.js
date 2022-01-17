e23f4bdd18c480ce124a6efae07641fa4505927a
oleg.bolshakov <oleg@bolshakov.net>
2022-01-17 13:08:09 +0300

    05-vue-router/01-AuthPages

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,6 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import IndexPage from '../views/PageIndex';
+import LoginPage from '../views/PageLogin';
+import RegisterPage from '../views/PageRegister';
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: IndexPage,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterPage,
+  },
+];
+
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  routes,
 });
