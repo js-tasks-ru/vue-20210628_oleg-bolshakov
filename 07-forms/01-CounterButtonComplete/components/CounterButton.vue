b601cf59626f282e3cbfaed648f87f48844e1bad
oleg.bolshakov <oleg@bolshakov.net>
2022-02-03 12:59:15 +0300

    07-forms/01-CounterButtonComplete

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,33 @@
 <template>
-  <button></button>
+  <button type="button" @click="handleClick">{{ localCopyCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+  data() {
+    return {
+      localCopyCount: null,
+    };
+  },
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
+  watch: {
+    count: {
+      handler: function(newValue) {
+        this.localCopyCount = newValue;
+      },
+      immediate: true,
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('update:count', ++this.localCopyCount);
+    }
+  }
 };
 </script>
