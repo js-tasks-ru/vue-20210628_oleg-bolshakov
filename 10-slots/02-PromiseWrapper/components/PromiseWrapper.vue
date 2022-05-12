42cacf5920cd9e4024a9951f912277fb6ae63685
oleg.bolshakov <oleg@bolshakov.net>
2022-05-12 17:08:14 +0300

    10-slots/02-promisewrapper

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,16 +1,40 @@
 <template>
-  <!-- -->
+  <slot :name="slotName" :result="result" :error="error" />
 </template>
 
 <script>
 export default {
   name: 'PromiseWrapper',
 
+  data() {
+    return {
+      slotName: null,
+      result: null,
+      error: null,
+    }
+  },
+  
   props: {
     promise: {
       type: Promise,
       required: true,
     },
   },
+
+  watch: {
+    promise: {
+      handler() {
+        this.slotName = 'pending';
+        this.promise.then(result => {
+            this.result = result;
+            this.slotName = 'fulfilled';
+          }).catch(error => {
+            this.error = error;
+            this.slotName = 'rejected';
+          })
+      },
+      immediate: true,
+    }
+  }
 };
 </script>
